2eefc0d73ea1b089c3df09805bad64762e3b0c7d
Ángel Cárdenas <angelcardenasrod@gmail.com>
2025-11-17 21:06:01 +0100

    feature/HU-FE-03 Configurar ESLint y Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import './App.css'
+import "./App.css";
 import HealthStatus from "./components/HealthStatus";
 function App() {
-
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-50 to-indigo-50 flex items-center justify-center p-6">
       <div className="bg-white shadow-lg rounded-xl max-w-md w-full p-8 flex flex-col items-center text-center">
         <h1 className="text-3xl md:text-4xl font-extrabold text-gray-900 mb-4">
           CofradíaGo Frontend
         </h1>
-        <p className="text-gray-700 text-lg mb-2">
-          ¡TailwindCSS está funcionando!
-        </p>
+        <p className="text-gray-700 text-lg mb-2">¡TailwindCSS está funcionando!</p>
         <div className="text-gray-700 text-lg mb-6">
           <HealthStatus />
         </div>
@@ -19,7 +16,7 @@ function App() {
         </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
